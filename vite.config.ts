409e26e82edbb1bc9d53c72ed764dd5649202ca3
Milind Mishra <milind.mishra4@gmail.com>
2025-12-10 10:58:19 +0530

    feat: update to pnpm and use nitro for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import viteReact from '@vitejs/plugin-react'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   server: {
@@ -14,6 +15,8 @@ export default defineConfig({
     tanstackStart({
       srcDirectory: 'src',
     }),
+    nitro(),
     viteReact(),
   ],
+  nitro: {},
 })
